3995c42eff6007bc39d055171c6ab92a9254e049
pashaessel <essel071@yandex.ru>
2022-05-13 11:45:57 +0300

    init

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
-const express = require('express');
-const config = require('./config')
-const app = express()
-const port = 3000
-const headers = {
-  'Access-Control-Allow-Origin': config.frontPath,
-  'Vary': 'Origin'
+import express from 'express';
+import campaignRouter from './routes/campaignsRouter.js'
+
+import { BACK_PATH, FRONT_PATH, PORT } from './config.js';
+
+const app = express();
+app.use(express.json()) // for parsing application/json
+
+//CORS 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+  app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+    res.send();
+  });
+});
+
+//Регистрируем роутер для кампаний
+app.use('/api/', campaignRouter);
+
+app.post('/api/sign-in', (req, res) => {
+
+})
+
+
+async function startApp() {
+  try {
+    app.listen(PORT, () => {
+      console.log(`SERVER STARTED ON PORT ${PORT}`)
+    })
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-app.get('/', (req, res) => {
-   res.set('Access-Control-Allow-Origin' ,'*')
-  res.send('test')
-  console.log(test);
-})
+startApp();
 
-app.get('/campaigns/create', (req, res) => {
-  res.set('Access-Control-Allow-Origin' , config.backPath)
-  res.send('test')
-  console.log(test);
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
